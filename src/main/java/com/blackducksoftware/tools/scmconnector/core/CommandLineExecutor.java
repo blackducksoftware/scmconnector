f0390a10585f086fa7d34e1a9db1cafca991a5cd
stevebillings <sbillings@blackducksoftware.com>
2016-01-19 11:05:57 -0500

    #2: Eliminate log msg that exposed password

--- src/main/java/com/blackducksoftware/tools/scmconnector/core/CommandLineExecutor.java
+++ src/main/java/com/blackducksoftware/tools/scmconnector/core/CommandLineExecutor.java
@@ -8,12 +8,12 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License version 2
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *******************************************************************************/
 package com.blackducksoftware.tools.scmconnector.core;
 
@@ -35,64 +35,69 @@ public class CommandLineExecutor implements ICommandLineExecutor {
 
     @Override
     public int executeCommand(Logger log, CommandLine command, File targetDir,
-	    String promptResponse) throws Exception {
-	DefaultExecutor executor = new DefaultExecutor();
-	executor.setExitValue(0);
+            String promptResponse) throws Exception {
+        DefaultExecutor executor = new DefaultExecutor();
+        executor.setExitValue(0);
 
-	PumpStreamHandler psh = new PumpStreamHandler(new ExecLogHangler(log,
-		Level.INFO));
-	executor.setStreamHandler(psh);
+        PumpStreamHandler psh = new PumpStreamHandler(new ExecLogHangler(log,
+                Level.INFO));
+        executor.setStreamHandler(psh);
 
-	providePromptResponse(psh, promptResponse);
+        providePromptResponse(psh, promptResponse);
 
-	executor.setWorkingDirectory(targetDir);
+        executor.setWorkingDirectory(targetDir);
 
-	int exitStatus = 1;
-	try {
-	    exitStatus = executor.execute(command,
-		    EnvironmentUtils.getProcEnvironment());
-	} catch (Exception e) {
-	    log.error("Failure executing Command: " + e.getMessage());
-	}
-	return exitStatus;
+        // This log msg would reveal password
+        // log.info("Command: " + command.toString() + " executed in: "
+        // + targetDir.toString());
+
+        int exitStatus = 1;
+        try {
+            exitStatus = executor.execute(command,
+                    EnvironmentUtils.getProcEnvironment());
+        } catch (Exception e) {
+            log.error("Failure executing Command: " + e.getMessage());
+        }
+        return exitStatus;
     }
 
     private void providePromptResponse(PumpStreamHandler psh,
-	    String promptResponse) throws IOException {
-	if (promptResponse == null) {
-	    return;
-	}
+            String promptResponse) throws IOException {
+        if (promptResponse == null) {
+            return;
+        }
 
-	OutputStream out = new ByteArrayOutputStream();
-	psh.setProcessInputStream(out);
-	String responseWithEOL = promptResponse + "\n";
-	out.write(responseWithEOL.getBytes());
+        OutputStream out = new ByteArrayOutputStream();
+        psh.setProcessInputStream(out);
+        String responseWithEOL = promptResponse + "\n";
+        out.write(responseWithEOL.getBytes());
     }
 
     @Override
     public int executeCommand(Logger log, CommandLine command, File targetDir)
-	    throws Exception {
-	return executeCommand(log, command, targetDir, null);
+            throws Exception {
+        return executeCommand(log, command, targetDir, null);
     }
 
     @Override
     public CommandResults executeCommandForOutput(Logger log,
-	    CommandLine command, File targetDir) throws Exception {
-	DefaultExecutor executor = new DefaultExecutor();
-	executor.setExitValue(0);
-	ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-	PumpStreamHandler psh = new PumpStreamHandler(outputStream);
-	executor.setStreamHandler(psh);
+            CommandLine command, File targetDir) throws Exception {
+        DefaultExecutor executor = new DefaultExecutor();
+        executor.setExitValue(0);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PumpStreamHandler psh = new PumpStreamHandler(outputStream);
+        executor.setStreamHandler(psh);
 
-	int exitStatus = 1;
-	executor.setWorkingDirectory(targetDir);
-	exitStatus = executor.execute(command);
-	String outputString = outputStream.toString();
+        int exitStatus = 1;
+        executor.setWorkingDirectory(targetDir);
+        exitStatus = executor.execute(command);
+        String outputString = outputStream.toString();
 
-	log.info("Command: " + command.toString() + "executed in: "
-		+ targetDir.toString() + "; output: " + outputString);
+        // This log msg would reveal password
+        // log.info("Command: " + command.toString() + " executed in: "
+        // + targetDir.toString() + "; output: " + outputString);
 
-	return new CommandResults(exitStatus, outputString);
+        return new CommandResults(exitStatus, outputString);
     }
 
 }
